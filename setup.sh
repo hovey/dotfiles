b058067211dc26bd34dfff47fa421411433a8cf3
Chad Hovey <hovey@me.com>
2025-06-04 10:03:59 -0600

    launch terminal and brave

--- setup.sh
+++ setup.sh
@@ -3,9 +3,28 @@
 echo "🔧 Configuring custom GNOME keyboard shortcuts..."
 
 gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>w']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-side-w "['<Shift><Alt>h']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-side-e "['<Shift><Alt>l']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-side-w "['<Super>Left', '<Shift><Alt>h']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-side-e "['<Super>Right', '<Shift><Alt>l']"
 gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Shift><Alt>Return']"
+
+# Define custom shortcut names
+CUSTOM_KEYBINDINGS_BASE="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
+KEY1="$CUSTOM_KEYBINDINGS_BASE/custom0/"
+KEY2="$CUSTOM_KEYBINDINGS_BASE/custom1/"
+
+# Register custom keybinding paths
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
+  "['$KEY1', '$KEY2']"
+
+# Set Brave browser launcher
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY1 name "Launch Brave"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY1 command "brave"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY1 binding "<Shift><Alt>b"
+
+# Set Terminal launcher
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY2 name "Launch Terminal"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY2 command "gnome-terminal"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY2 binding "<Shift><Alt>t"
 
 echo "✓ GNOME keybindings updated"
 
