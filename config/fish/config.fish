af72c99b07cf328de232d210a8a1633240457f57
Chad Hovey <hovey@me.com>
2025-08-22 12:23:45 -0400

    some additional notes on starship

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -70,4 +70,8 @@ end
 if type -q starship
     starship init fish | source
 end
+#
+# The above commands tell Fish to use Starship, https://starship.rs/, 
+# instead of fish_prompt (in ~/.config/fish/config.fish), to create the prompt.
+# the Starship configuration lives at ~/.config/starship.toml
 
